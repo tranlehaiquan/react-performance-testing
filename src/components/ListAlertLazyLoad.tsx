3196a597a76f85071aa86c86bc0c22b90fd15313
Q.Tran <quan.tran@metro.digital>
2025-12-07 13:22:04 +0700

    Refactor ListAlertLazyLoad to use react-window's List component for dynamic row height
    - Replaced FixedSizeList with List for better performance with dynamic row heights.
    - Moved ItemRenderer function outside of ListAlertLazyLoad for clarity and reusability.
    - Integrated useDynamicRowHeight hook to manage row heights dynamically.
    - Updated rendering logic to pass necessary props to ItemRenderer.

--- src/components/ListAlertLazyLoad.tsx
+++ src/components/ListAlertLazyLoad.tsx
@@ -1,7 +1,28 @@
 import { useState, useCallback } from "react";
 import AlertDemoMemo, { AlertDemo } from "@/components/SomeAlert";
 import { AddItem } from "@/AddItem";
-import { FixedSizeList } from "react-window";
+import { List, RowComponentProps, useDynamicRowHeight } from "react-window";
+
+const ItemRenderer = ({
+  index,
+  style,
+  list,
+  memo,
+}: RowComponentProps<{
+  list: any[];
+  memo: boolean;
+}>) => {
+  const item = list[index];
+  return (
+    <div style={style}>
+      {memo ? (
+        <AlertDemoMemo content={item.id.toString()} id={item.id} />
+      ) : (
+        <AlertDemo content={item.id.toString()} id={item.id} />
+      )}
+    </div>
+  );
+};
 
 function ListAlertLazyLoad({ memo = false }: { memo?: boolean }) {
   const [list, setList] = useState<
@@ -18,18 +39,9 @@ function ListAlertLazyLoad({ memo = false }: { memo?: boolean }) {
     setList((prev) => [...newItem, ...prev]);
   }, []);
 
-  const ItemRenderer = ({ index, style }: { index: number; style: any }) => {
-    const item = list[index];
-    return (
-      <div style={style}>
-        {memo ? (
-          <AlertDemoMemo content={item.id.toString()} id={item.id} />
-        ) : (
-          <AlertDemo content={item.id.toString()} id={item.id} />
-        )}
-      </div>
-    );
-  };
+  const rowHeight = useDynamicRowHeight({
+    defaultRowHeight: 50,
+  });
 
   return (
     <div className="flex flex-col w-full h-screen p-2">
@@ -40,17 +52,12 @@ function ListAlertLazyLoad({ memo = false }: { memo?: boolean }) {
         </p>
       </div>
 
-      <div className="flex-grow overflow-y-auto grid gap-2 content-start">
-        <FixedSizeList
-          height={800}
-          width="100%"
-          itemCount={list.length}
-          itemSize={80}
-          overscanCount={5}
-        >
-          {ItemRenderer}
-        </FixedSizeList>
-      </div>
+      <List
+        rowComponent={ItemRenderer}
+        rowCount={list.length}
+        rowHeight={rowHeight}
+        rowProps={{ list, memo }}
+      />
     </div>
   );
 }
